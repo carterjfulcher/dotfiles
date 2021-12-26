79721cc12f94cf64e0b40a1f17c9b7ea54940e23
carterjfulcher <carter@fulchereng.com>
2021-12-25 21:52:25 -0700

    remove unused config files

--- polybar/scripts/date.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-
-date=$(date '+%a %e %b %k:%M')
-
-echo "󰧒 %{T2}$date%{T-}"
--- polybar/scripts/temperature.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-import os 
-
-data = os.popen('sensors').readlines()
-print(data[15].replace('(high = +100.0°C, crit = +100.0°C)', '').replace('Package id 0:  +', ''))
-
-
-
